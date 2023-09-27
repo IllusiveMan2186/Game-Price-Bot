37c34b2cb5e1b9afb6963dc1a08e518598579975
IllusiveMan2186 <49444101+IllusiveMan2186@users.noreply.github.com>
2023-09-27 14:27:04 +0300

    GPB-8 - Exception Handler

---
+++ gpb-web/src/main/java/com/gpb/web/controller/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.gpb.web.controller;
+
+import com.gpb.web.exception.EmailAlreadyExistException;
+import com.gpb.web.exception.NotFoundException;
+import com.gpb.web.exception.UrlAlreadyExistException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {EmailAlreadyExistException.class, UrlAlreadyExistException.class})
+    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
