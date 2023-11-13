7debeab6737c3e08cd43dc88a466f6654216942f
jediks <jediks22@gmail.com>
2023-11-13 12:51:03 +0200

    Remove all img

--- gpb-front/src/components/GameImage.js
+++ gpb-front/src/components/GameImage.js
@@ -4,22 +4,22 @@ import Message from './Message';
 
 
 export function GameImage(props) {
-    let image 
-    try {
-        image = require(`../img/${props.gameName}.jpg`)
-      } catch {
-        image = defaultImage
-      }
-       
-      return (<img class={props.className} src={image} on ></img>);
+  let image
+  try {
+    image = require(`../img/${props.gameName}.jpg`)
+  } catch {
+    image = defaultImage
+  }
+
+  return (<img class={props.className} src={image} on ></img>);
 }
 
 export function GameAvailability(props) {
   return (
-      <div class={props.available ? "App-game-content-list-game-info-available" 
+    <div class={props.available ? "App-game-content-list-game-info-available"
       : "App-game-content-list-game-info-available not-available"}>
-          {props.available ? <Message string={'app.game.is.available'} />
-              : <Message string={'app.game.not.available'} />}
-      </div>
+      {props.available ? <Message string={'app.game.is.available'} />
+        : <Message string={'app.game.not.available'} />}
+    </div>
   )
 }
