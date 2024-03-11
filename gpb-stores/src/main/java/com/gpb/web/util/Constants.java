ca221b51559d64f591138d958d9b5ef8128c7428
IllusiveMan2186 <49444101+IllusiveMan2186@users.noreply.github.com>
2024-03-11 15:49:49 +0200

    GPB-84 - Fix Google chrome driver for selenium
    https://game-price-bot.atlassian.net/browse/GPB-84

--- gpb-stores/src/main/java/com/gpb/web/util/Constants.java
+++ gpb-stores/src/main/java/com/gpb/web/util/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 
     public static final String USER_ROLE = "ROLE_USER";
     public static final String JPG_IMG_FILE_EXTENSION = ".jpg";
+    public static final String GAME_INFO_CHANGE_CHECKING_TIME = "0 04 00 * * *";//4 am
     public static final String EMAIL_SERVICE_TOPIC = "gpb_email_event";
     public static final String GAME_NAME_SEARCH_TOPIC = "gpb_game_name_search_request";
     public static final String GAME_URL_SEARCH_TOPIC = "gpb_game_url_search_request";
