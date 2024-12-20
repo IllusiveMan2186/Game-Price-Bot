bbdcc8e340eb53738c73a4f0686db2d0da7a6621
jediks <jediks22@gmail.com>
2024-12-20 13:51:08 +0200

    Change footer UI

--- gpb-front/src/components/common/layout/Footer/Footer.jsx
+++ gpb-front/src/components/common/layout/Footer/Footer.jsx
@@ -28,48 +28,9 @@ export default class Footer extends React.Component {
                                         <i className="fas fa-envelope mr-3" /> info@gmail.com
                                     </p>
                                 </div>
-                                <div id="footer-media-info" className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                    <h6 className="text-uppercase mb-4 font-weight-bold">
-                                        <Message string={'app.footer.follow'} />
-                                    </h6>
-                                    <a
-                                        className="btn btn-primary btn-floating m-1"
-                                        style={{ backgroundColor: "#dd4b39" }}
-                                        href="#!"
-                                        role="button"
-                                    >
-                                        <i className="fab fa-google" />
-                                    </a>
-                                    <a
-                                        className="btn btn-primary btn-floating m-1"
-                                        style={{ backgroundColor: "#0082ca" }}
-                                        href="#!"
-                                        role="button"
-                                    >
-                                        <i className="fab fa-linkedin-in" />
-                                    </a>
-                                    <a
-                                        className="btn btn-primary btn-floating m-1"
-                                        style={{ backgroundColor: "#333333" }}
-                                        href="#!"
-                                        role="button"
-                                    >
-                                        <i className="fab fa-github" />
-                                    </a>
-                                </div>
                                 {<Localization />}
                             </div>
                         </section>
-                    </div>
-                    
-                    <div
-                        className="text-center p-3"
-                        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-                    >
-                        © 2020 Copyright:
-                        <a className="text-white" href="https://mdbootstrap.com/">
-                            MDBootstrap.com
-                        </a>
                     </div>
                 </footer>
             </>
