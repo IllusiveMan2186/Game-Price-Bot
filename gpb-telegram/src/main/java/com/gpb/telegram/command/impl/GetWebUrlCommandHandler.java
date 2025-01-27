bdafcafe1783201c4c70b13b08048e6dc7295840
IllusiveMan2186 <49444101+IllusiveMan2186@users.noreply.github.com>
2025-01-27 15:37:44 +0200

    GPBN-34 - Auto link after redirection from telegram to web
    https://gpbn.atlassian.net/browse/GPBN-34

--- gpb-telegram/src/main/java/com/gpb/telegram/command/impl/GetWebUrlCommandHandler.java
+++ gpb-telegram/src/main/java/com/gpb/telegram/command/impl/GetWebUrlCommandHandler.java
@@ -1,8 +1,11 @@
 package com.gpb.telegram.command.impl;
 
+import com.gpb.common.service.UserLinkerService;
 import com.gpb.telegram.command.CommandHandler;
 import com.gpb.telegram.entity.TelegramRequest;
 import com.gpb.telegram.entity.TelegramResponse;
+import com.gpb.telegram.filter.FilterChainMarker;
+import com.gpb.telegram.util.Constants;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
@@ -12,15 +15,17 @@ import java.util.Locale;
 
 @Data
 @Component("url")
+@FilterChainMarker(Constants.USER_EXISTING_FILTER)
 public class GetWebUrlCommandHandler implements CommandHandler {
 
     @Value("${FRONT_SERVICE_URL}")
     private String frontendServiceUrl;
-
     private final MessageSource messageSource;
+    private final UserLinkerService userLinkerService;
 
-    public GetWebUrlCommandHandler(MessageSource messageSource) {
+    public GetWebUrlCommandHandler(MessageSource messageSource, UserLinkerService userLinkerService) {
         this.messageSource = messageSource;
+        this.userLinkerService = userLinkerService;
     }
 
     @Override
@@ -30,6 +35,8 @@ public class GetWebUrlCommandHandler implements CommandHandler {
 
     @Override
     public TelegramResponse apply(TelegramRequest request) {
-        return new TelegramResponse(request, frontendServiceUrl);
+        String url = frontendServiceUrl + Constants.SET_LINK_TOKEN +
+                userLinkerService.getAccountsLinkerToken(request.getUserBasicId());
+        return new TelegramResponse(request, url);
     }
 }
--- gpb-telegram/src/test/java/com/gpb/telegram/command/impl/GetWebUrlCommandHandlerTest.java
+++ gpb-telegram/src/test/java/com/gpb/telegram/command/impl/GetWebUrlCommandHandlerTest.java
@@ -1,7 +1,10 @@
 package com.gpb.telegram.command.impl;
 
+import com.gpb.common.service.UserLinkerService;
 import com.gpb.telegram.entity.TelegramRequest;
 import com.gpb.telegram.entity.TelegramResponse;
+import com.gpb.telegram.entity.TelegramUser;
+import com.gpb.telegram.util.Constants;
 import com.gpb.telegram.util.UpdateCreator;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.MessageSource;
@@ -17,8 +20,8 @@ import static org.mockito.Mockito.when;
 class GetWebUrlCommandHandlerTest {
 
     MessageSource messageSource = mock(MessageSource.class);
-
-    GetWebUrlCommandHandler controller = new GetWebUrlCommandHandler(messageSource);
+    UserLinkerService userLinkerService = mock(UserLinkerService.class);
+    GetWebUrlCommandHandler controller = new GetWebUrlCommandHandler(messageSource, userLinkerService);
 
     @Test
     void testGetDescription_whenSuccess_shouldReturnDescription() {
@@ -34,10 +37,18 @@ class GetWebUrlCommandHandlerTest {
     void testApply_whenSuccess_shouldReturnUrl() {
         String chatId = "123456";
         String url = "http://mocked-url.com";
+        String token = "token";
+        long basicUserId = 123L;
         Update update = UpdateCreator.getUpdateWithoutCallback("/url ", Long.parseLong(chatId));
+        TelegramUser user = TelegramUser.builder()
+                .basicUserId(basicUserId)
+                .build();
         TelegramRequest request = TelegramRequest.builder()
                 .update(update)
+                .user(user)
                 .build();
+        when(userLinkerService.getAccountsLinkerToken(basicUserId)).thenReturn(token);
+
 
         controller.setFrontendServiceUrl(url);
 
@@ -47,6 +58,6 @@ class GetWebUrlCommandHandlerTest {
 
         SendMessage message = (SendMessage) response.getMessages().get(0);
         assertEquals(chatId, message.getChatId());
-        assertEquals(url, message.getText());
+        assertEquals(url + Constants.SET_LINK_TOKEN + token, message.getText());
     }
 }
