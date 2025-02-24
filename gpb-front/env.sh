a1053646d77eef690b2146bc42091154fe56a55d
jediks <jediks22@gmail.com>
2025-02-24 14:02:48 +0200

    env sh file removing

--- gpb-front/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-echo "window._env_ = {" > /usr/share/nginx/html/env.js
-echo "  BACKEND_SERVICE_URL: \"${BACKEND_SERVICE_URL}\"," >> /usr/share/nginx/html/env.js
-echo "  TELEGRAM_BOT_URL: \"${TELEGRAM_BOT_URL}\"," >> /usr/share/nginx/html/env.js
-echo "  SUPPORT_EMAIL: \"${SUPPORT_EMAIL}\"" >> /usr/share/nginx/html/env.js
-echo "};" >> /usr/share/nginx/html/env.js
-
-exec "$@"
