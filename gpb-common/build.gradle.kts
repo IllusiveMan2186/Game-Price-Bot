5f3ab0391c3846b7b7c445d6677298d1ac670075
jediks <jediks22@gmail.com>
2025-05-15 22:44:49 +0300

    New release

--- gpb-common/build.gradle.kts
+++ gpb-common/build.gradle.kts
@@ -13,7 +13,7 @@ fun getEnvOrProperty(name: String): String {
 }
 
 group = "com.gpb"
-version = "1.2.1"
+version = "1.3.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
--- gpb-game/build.gradle.kts
+++ gpb-game/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.gpb"
-version = "1.2.1"
+version = "1.3.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 fun getEnvOrProperty(name: String): String {
@@ -35,7 +35,7 @@ configurations.all {
 }
 
 dependencies {
-    implementation("com.gpb:common:1.2.1")
+    implementation("com.gpb:common:1.3.0")
 
     implementation("commons-io:commons-io:2.11.0")
     implementation("com.auth0:java-jwt:4.3.0")
