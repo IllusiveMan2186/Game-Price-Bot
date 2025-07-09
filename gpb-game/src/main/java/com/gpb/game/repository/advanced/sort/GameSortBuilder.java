fe539ad7f333e98084df9942571683c0f6a9bd71
jediks <jediks22@gmail.com>
2025-07-09 22:06:02 +0300

    Add javadoc

--- gpb-game/src/main/java/com/gpb/game/repository/advanced/sort/GameSortBuilder.java
+++ gpb-game/src/main/java/com/gpb/game/repository/advanced/sort/GameSortBuilder.java
@@ -14,11 +14,24 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility component for applying dynamic sorting to JPA Criteria queries involving {@link Game} entities.
+ * <p>
+ * Supports basic field sorting and special handling for aggregated discount prices from the {@link GameInShop} relation.
+ */
 @Component
 public class GameSortBuilder {
 
     private static final String DISCOUNT_PRICE_PROPERTY = "gamesInShop.discountPrice";
 
+    /**
+     * Applies sorting to the given CriteriaQuery based on the Sort information from {@link Pageable}.
+     *
+     * @param query    the CriteriaQuery to apply sorting to
+     * @param cb       the CriteriaBuilder used to construct Order expressions
+     * @param gameRoot the root entity in the query (i.e., {@link Game})
+     * @param pageable the Pageable containing sort information
+     */
     public void applySorting(CriteriaQuery<Game> query, CriteriaBuilder cb, Root<Game> gameRoot, Pageable pageable) {
         if (!pageable.getSort().isSorted()) {
             return;
